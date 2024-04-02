a0261834af42c039a982b1517953883851c3b4f0
Joseph_Thairu <joejoement@gmail.com>
2024-04-02 13:50:19 +0300

    to do list deliverables added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e)=>{
+    e.preventDefault()
+    handleToDo(e.target.newtaskdescription.value)
+  }) 
 });
+
+function handleToDo(todo){
+      const output = document.createElement('li')
+      const btn = document.createElement('button')
+      btn.addEventListener('click', deleteToDo)
+      output.textContent = `${todo}    `
+      btn.textContent = 'X'
+      output.appendChild(btn)
+      document.querySelector('#tasks').appendChild(output)
+
+}
+
+function deleteToDo(e){
+  e.target.parentNode.remove()
+}
